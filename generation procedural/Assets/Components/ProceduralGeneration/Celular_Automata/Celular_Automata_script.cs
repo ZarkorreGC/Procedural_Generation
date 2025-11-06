9d224ce27322a711ff29615209a67775f8deec7d
ZarkorreGC <nponthieu@gaming.tech>
2025-11-06 10:29:13 +0100

    push

--- generation procedural/Assets/Components/ProceduralGeneration/Celular_Automata/Celular_Automata_script.cs
+++ generation procedural/Assets/Components/ProceduralGeneration/Celular_Automata/Celular_Automata_script.cs
@@ -2,6 +2,7 @@ using Components.ProceduralGeneration;
 using Cysharp.Threading.Tasks;
 using JetBrains.Annotations;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
@@ -14,7 +15,7 @@ using VTools.ScriptableObjectDatabase;
 
 public class Celular_Automata_script : ProceduralGenerationMethod
 {
-    [SerializeField] public List<int> noise;
+    [SerializeField] public List<List<int>> noise;
     [SerializeField] public int noiseIntensity;
 
     protected override async UniTask ApplyGeneration(CancellationToken cancellationToken)
@@ -22,7 +23,7 @@ public class Celular_Automata_script : ProceduralGenerationMethod
         // Declare variables here
         // ........
         //Debug.Log("test algo");
-        noise = new List<int>();
+        noise = new List<List<int>>();
 
         
 
@@ -37,6 +38,8 @@ public class Celular_Automata_script : ProceduralGenerationMethod
 
             // Your algorithm here
             // .......
+
+            UpdateMap();
 
 
             // Waiting between steps to see the result.
@@ -58,7 +61,7 @@ public class Celular_Automata_script : ProceduralGenerationMethod
             {
                 if (noiseIntensity == 100)
                 {
-                    noise.Add(1);
+                    noise[x].Add(1);
                     
                 }
                 else
@@ -82,7 +85,6 @@ public class Celular_Automata_script : ProceduralGenerationMethod
 
     private void BuildNoise()
     {
-        var groundTemplate = ScriptableObjectDatabase.GetScriptableObject<GridObjectTemplate>("Room");
         int i = 0;
 
         // Instantiate ground blocks
@@ -110,6 +112,61 @@ public class Celular_Automata_script : ProceduralGenerationMethod
                 i++;
             }
         }
+    }
+
+    private void UpdateMap()
+    {
+        int i = 0;
+
+        for (int x = 0; x < Grid.Width; x++)
+        {
+            for (int z = 0; z < Grid.Lenght; z++)
+            {
+                Checkaround(x,z);
+
+
+                i++;
+            }
+        }
+
+    }
+
+    private void Checkaround(int x, int z)
+    {
+        int _countGrass = 0;
+
+        for ( int ix = x - 1 ; x < ix + 1; x++)
+        {
+            for (int iz = z - 1; z < iz + 1; z++)
+            {
+                bool[,] isGround = new bool[Grid.Width, Grid.Lenght];
+
+                if(!Grid.TryGetCellByCoordinates(ix, iz, out Cell cell))
+                {
+                    isGround[ix, iz] = cell.GridObject.Template.Name == GRASS_TILE_NAME;
+                    if (isGround[ix, iz])
+                    {
+                        _countGrass++;
+                    }
+                }
+            }
+        }
+
+        if (_countGrass >= 4)
+        {
+            if (!Grid.TryGetCellByCoordinates(x, z, out Cell cell))
+            {
+                AddTileToCell(cell, GRASS_TILE_NAME, true);
+            }
+        }
+        else
+        {
+            if (!Grid.TryGetCellByCoordinates(x, z, out Cell cell))
+            {
+                AddTileToCell(cell, WATER_TILE_NAME, true);
+            }
+        }
+
     }
 
     private void BuildGround()
